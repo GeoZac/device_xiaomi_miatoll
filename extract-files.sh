0501afc6012093f82402fa2f0e06653b4c750cbe
Michael Bestas <mkbestas@lineageos.org>
2021-02-14 15:05:51 +0100

    miatoll: Switch to standalone extract utils
    Align with templates while we are at it
    
    Change-Id: Ife50623a3d1b8733f5cc76995c982d8950d39ad4

--- extract-files.sh
+++ extract-files.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -17,7 +18,5 @@ set -e
 export DEVICE=miatoll
 export DEVICE_COMMON=sm6250-common
 export VENDOR=xiaomi
-
-export DEVICE_BRINGUP_YEAR=2020
 
 "./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
